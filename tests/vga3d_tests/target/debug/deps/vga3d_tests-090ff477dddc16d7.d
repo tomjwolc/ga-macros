4e2275714551744517ac2412abb4321c86cee7a8
Tom Wolcott <tomjwolc@gmail.com>
2022-06-30 09:35:48 -0400

    v0.1.0

---
+++ tests/vga3d_tests/target/debug/deps/vga3d_tests-090ff477dddc16d7.d
@@ -0,0 +1,5 @@
+/Users/tomwolcott/Desktop/rust_files/macros/tests/vga3d_tests/target/debug/deps/vga3d_tests-090ff477dddc16d7.rmeta: src/main.rs
+
+/Users/tomwolcott/Desktop/rust_files/macros/tests/vga3d_tests/target/debug/deps/vga3d_tests-090ff477dddc16d7.d: src/main.rs
+
+src/main.rs:
---
+++ tests/vga3d_tests/target/debug/deps/vga3d_tests-e4129356c9f4bc51.d
@@ -0,0 +1,5 @@
+/Users/tomwolcott/Desktop/rust_files/macros/tests/vga3d_tests/target/debug/deps/vga3d_tests-e4129356c9f4bc51.rmeta: src/main.rs
+
+/Users/tomwolcott/Desktop/rust_files/macros/tests/vga3d_tests/target/debug/deps/vga3d_tests-e4129356c9f4bc51.d: src/main.rs
+
+src/main.rs:
