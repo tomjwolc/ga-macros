08c7bbaaae699660576fd764c1ec81c1832111e2
Tom Wolcott <tomjwolc@gmail.com>
2022-08-04 08:51:13 -0400

    quick fix

--- tests/pga3d_tests/target/debug/deps/ga_macros-e621dd7da9324b34.d
+++ tests/pga3d_tests/target/debug/deps/ga_macros-e621dd7da9324b34.d
@@ -1,6 +1,6 @@
-/Users/tomwolcott/Desktop/rust_files/macros/tests/pga3d_tests/target/debug/deps/libga_macros-e621dd7da9324b34.dylib: /Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/2ec6719/src/lib.rs /Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/2ec6719/src/algebra.rs
+/Users/tomwolcott/Desktop/rust_files/macros/tests/pga3d_tests/target/debug/deps/libga_macros-e621dd7da9324b34.dylib: /Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/145d757/src/lib.rs /Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/145d757/src/algebra.rs
 
-/Users/tomwolcott/Desktop/rust_files/macros/tests/pga3d_tests/target/debug/deps/ga_macros-e621dd7da9324b34.d: /Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/2ec6719/src/lib.rs /Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/2ec6719/src/algebra.rs
+/Users/tomwolcott/Desktop/rust_files/macros/tests/pga3d_tests/target/debug/deps/ga_macros-e621dd7da9324b34.d: /Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/145d757/src/lib.rs /Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/145d757/src/algebra.rs
 
-/Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/2ec6719/src/lib.rs:
-/Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/2ec6719/src/algebra.rs:
+/Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/145d757/src/lib.rs:
+/Users/tomwolcott/.cargo/git/checkouts/ga-macros-fa1a5914827173dc/145d757/src/algebra.rs:
